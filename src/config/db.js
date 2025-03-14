d40c3a06a8b94690ca680757fcd566468e20cffe
wanifaisal <wanifaisal155223@gmail.com>
2025-03-14 22:35:13 +0530

    jmnfkjs,nfk,mnfknf

--- src/config/db.js
+++ src/config/db.js
@@ -5,7 +5,14 @@ dotenv.config();
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI);
+        await mongoose.connect(process.env.MONGO_URI,{
+            
+                useNewUrlParser: true,
+                useUnifiedTopology: true,
+                serverSelectionTimeoutMS: 5000, // Fails if no response in 5s
+                socketTimeoutMS: 45000, // Keeps connection alive for 45s
+            
+        });
         console.log('MongoDB connected...');
     } catch (err) {
         console.error('MongoDB connection error:', err.message);
