2f431b339326f8e63d9120fde29697a8926f76f9
wanifaisal <wanifaisal155223@gmail.com>
2025-03-19 20:27:38 +0530

    updated cores

--- index.js
+++ index.js
@@ -2,27 +2,31 @@
 const express = require("express");
 const webhookRoutes = require("./routes/webhooks"); // Import webhook routes
 const authRoute = require("./routes/auth");
-// const cors = require("cors");
+const cors = require("cors");
 require("dotenv").config();
 const userRoutes = require("./routes/users");
 const accountsRoutes = require("./routes/accounts");
 const app = express();
 app.use(express.json());
 // Use webhook routes
-// const allowedOrigins = ["http://localhost:5174", "http://localhost:5173"];
-// app.use(
-//   cors({
-//     origin: function (origin, callback) {
-//       if (allowedOrigins.indexOf(origin) !== -1) {
-//         callback(null, true);
-//       } else {
-//         callback(new Error("Not allowed by CORS"));
-//       }
-//     },
-//     methods: ["GET", "POST", "PUT", "DELETE"],
-//     credentials: true,
-//   })
-// );
+const allowedOrigins = [
+  "http://localhost:5174",
+  "http://localhost:5173",
+  "http://unifistruct.structuredsettlement.agency",
+];
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (allowedOrigins.indexOf(origin) !== -1) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 app.use("/api/ghl-contacts", webhookRoutes);
 app.use("/api/user_account", accountsRoutes);
 app.use("/api/login", authRoute);
